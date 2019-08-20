ef227e3e3cf24cb331502236f33832a5237dc7c0
Reese Kunz <reesekunz21@gmail.com>
2019-08-19 22:47:15 -0700

    added led testing for dispaly, finished mvp

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,29 +6,39 @@ import { render, fireEvent } from "@testing-library/react";
 import Display from "./Display";
 
 describe("<Display />", () => {
-    it("matches snapshot", () => {
-      const tree = renderer.create(<Display />);
-      expect(tree.toJSON()).toMatchSnapshot();
-    });
-    it("displays when gate is closed", () => {
-        const { getByText} = render(<Display locked={true} closed={true} />);
-        expect(getByText(/closed/i)).toBeTruthy();
-      });
-    
-      it("displays when gate is open", () => {
-        const { getByText } = render(<Display locked={true} closed={false} />);
-        expect(getByText(/open/i)).toBeTruthy();
-      });
-    
-      it("displays when gate is locked", () => {
-        const { getByText } = render(<Display locked={true} closed={false} />);
-        expect(getByText(/locked/i)).toBeTruthy();
-      });
-    
-      it("displays when gate is unlocked", () => {
-        const { getByText } = render(<Display locked={false} closed={false} />);
-        expect(getByText(/unlocked/i)).toBeTruthy();
-      });
-    
-});
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Display />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+  it("displays when gate is closed", () => {
+    const { getByText } = render(<Display locked={true} closed={true} />);
+    expect(getByText(/closed/i)).toBeTruthy();
+  });
 
+  it("displays when gate is open", () => {
+    const { getByText } = render(<Display locked={true} closed={false} />);
+    expect(getByText(/open/i)).toBeTruthy();
+  });
+
+  it("displays when gate is locked", () => {
+    const { getByText } = render(<Display locked={true} closed={false} />);
+    expect(getByText(/locked/i)).toBeTruthy();
+  });
+
+  it("displays when gate is unlocked", () => {
+    const { getByText } = render(<Display locked={false} closed={false} />);
+    expect(getByText(/unlocked/i)).toBeTruthy();
+  });
+
+  it("displays red led when gate is locked or closed ", () => {
+    const { getByText } = render(<Display locked={true} closed={true} />);
+    expect(getByText(/closed/i)).toBeTruthy();
+    expect(getByText(/locked/i)).toBeTruthy();
+  });
+
+  it("displays green led when gate is unlocked or open ", () => {
+    const { getByText } = render(<Display locked={false} closed={false} />);
+    expect(getByText(/open/i)).toBeTruthy();
+    expect(getByText(/unlocked/i)).toBeTruthy();
+  });
+});
